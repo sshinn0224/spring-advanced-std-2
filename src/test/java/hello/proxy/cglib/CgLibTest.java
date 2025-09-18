f69efb8d01ad80b5e47a7e5f230e60295f7e8d20
Jade-jaeho.shin <jaeho.shin@lulumedic.com>
2025-09-18 10:26:56 +0900

    chap ~ 60

---
+++ src/test/java/hello/proxy/cglib/CgLibTest.java
@@ -0,0 +1,26 @@
+package hello.proxy.cglib;
+
+import hello.proxy.cglib.code.TimeMethodInterceptor;
+import hello.proxy.common.service.ConcreteService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+@Slf4j
+public class CgLibTest {
+
+    @Test
+    void cglibTest() {
+        ConcreteService target = new ConcreteService();
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(ConcreteService.class);
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        ConcreteService proxy = (ConcreteService) enhancer.create();
+
+        log.info("target class = {}", target.getClass());
+        log.info("proxy class = {}", proxy.getClass());
+
+        proxy.call();
+    }
+}
